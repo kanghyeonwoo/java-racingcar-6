56ca1945ecb3fbb0e0984506050c3b54b609bc0e
kanghyeonwoo <150929120+kanghyeonwoo@users.noreply.github.com>
2023-12-03 14:23:46 +0900

    기능구현 목록 작성
    소스코드 작성 전 1차 기능구현 예상 목록 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import java.util.List;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        new random
     }
 }
